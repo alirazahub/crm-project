f7ac709cd246cbd17c3e6612c2a3946bdfa4ecf2
HafsaSaleem069 <hafsasaleem152ics@gmail.com>
2025-07-28 17:52:18 +0500

    Update usermodel.js

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 // Define the User Schema
 const UserSchema = new mongoose.Schema({
@@ -10,4 +10,5 @@ const UserSchema = new mongoose.Schema({
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+export default User;
