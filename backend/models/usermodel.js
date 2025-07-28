416867bd40e1b0a89f1882e3bcc3494435108736
HafsaSaleem069 <hafsasaleem152ics@gmail.com>
2025-07-28 15:39:34 +0500

    Added usermodel

---
+++ backend/models/usermodel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+// Define the User Schema
+const UserSchema = new mongoose.Schema({
+    fullname: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String }, // may be null for Google-only users
+    phone: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+});
+
+module.exports = mongoose.model("User", UserSchema);
